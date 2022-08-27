db7bb43c6b7960486d3bd438575d3b82d2d1ba53
DPPrarthana <111065819+DPPrarthana@users.noreply.github.com>
2022-08-27 19:28:59 +0530

    Add files via upload

---
+++ positive numbers in range.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list = [12,-7,5,64,-14]
+for i in list:
+    if i<0:
+        list.remove(i)
+print(list)
+
+
+# In[2]:
+
+
+list = [12,14,-95,3]
+for i in list:
+    if i<0:
+        list.remove(i)
+print(list)
+
+
+# In[ ]:
+
+
+
+
